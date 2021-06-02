d5f2da4f665e7757f1802ca62e55bfb504110a3d
Chung Chung <nguyenthichung997@gmail.com>
2021-06-02 10:17:42 +0700

    sua

---
+++ module3/case_study_02/furama_resort/src/model/bean/Employee.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Employee {
+    private int id;
+    private String fullName;
+
+    public Employee(int id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
